0566948ef6fb48e7bed25ca5f8e57a1c162ca2d2
sun <sa139339@gmail.com>
2023-11-08 15:31:18 +0900

    model change lstm -> gru

--- main.py
+++ main.py
@@ -1,167 +1,155 @@
-import numpy as np
-import pandas as pd
+import torch
+import torch.nn
+import torchvision.transforms as transforms
+import torchvision.datasets
+from torch.autograd import Variable
+from torch.nn import Parameter
+import torch.nn as nn
+from torch import Tensor
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import math
+
+import matplotlib.pyplot as plt
+from datetime import datetime
+import seaborn as sns
+
+
+class GRU(nn.Module) :
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length) :
+        super(GRU, self).__init__()
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.seq_length = seq_length
+        
+        self.gru = nn.GRU(input_size=input_size,hidden_size=hidden_size,
+                         num_layers=num_layers,batch_first=True)
+        self.fc_1 = nn.Linear(hidden_size, 128)
+        self.fc = nn.Linear(128, num_classes)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x) :
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        output, (hn) = self.gru(x, (h_0))
+        hn = hn.view(-1, self.hidden_size)
+        out = self.relu(hn)
+        out = self.fc_1(out)
+        out = self.relu(out)
+        out = self.fc(out)
+        return out
+
+
+
+plt.style.use('seaborn-whitegrid')
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+cuda = True if torch.cuda.is_available() else False
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+torch.manual_seed(125)
+
+if torch.cuda.is_available() :
+    torch.cuda.manual_seed_all(125)
+
 
 import yfinance as yf
-stock_data = yf.download('005930.KS',
-                     start='2010-01-01',
-                     end='2023-11-07') # 005930 : 삼성전자 주가
-stock_data.reset_index(inplace= True)
-print(stock_data)
+df = yf.download('005930.KS',
+                     start='2022-01-01',
+                     end='2022-05-31') # 005930 : 삼성전자 주가
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.optimizers import Adam
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-
-stock_data.drop(['Adj Close'], axis=1, inplace=True) # delete adjusted close
-
-# save original 'Open' prices for later
-original_open = stock_data['Open'].values
-
-# separate dates for future plotting
-dates = pd.to_datetime(stock_data['Date'])
-
-# variables for training
-cols = list(stock_data)[1:6]
-
-# new dataframe with only training data - 5 columns
-stock_data = stock_data[cols].astype(float)
-
-# normalize the dataset
-scaler = StandardScaler()
-scaler = scaler.fit(stock_data)
-stock_data_scaled = scaler.transform(stock_data)
-
-# split to train data and test data
-n_train = int(0.9*stock_data_scaled.shape[0])
-train_data_scaled = stock_data_scaled[0: n_train]
-train_dates = dates[0: n_train]
-
-test_data_scaled = stock_data_scaled[n_train:]
-test_dates = dates[n_train:]
-# print(test_dates.head(5))
-
-# data reformatting for LSTM
-pred_days = 1  # prediction period
-seq_len = 14   # sequence length = past days for future prediction.
-input_dim = 5  # input_dimension = ['Open', 'High', 'Low', 'Close', 'Volume']
-
-trainX = []
-trainY = []
-testX = []
-testY = []
-
-for i in range(seq_len, n_train-pred_days +1):
-    trainX.append(train_data_scaled[i - seq_len:i, 0:train_data_scaled.shape[1]])
-    trainY.append(train_data_scaled[i + pred_days - 1:i + pred_days, 0])
-
-for i in range(seq_len, len(test_data_scaled)-pred_days +1):
-    testX.append(test_data_scaled[i - seq_len:i, 0:test_data_scaled.shape[1]])
-    testY.append(test_data_scaled[i + pred_days - 1:i + pred_days, 0])
-
-trainX, trainY = np.array(trainX), np.array(trainY)
-testX, testY = np.array(testX), np.array(testY)
-
-# print(trainX.shape, trainY.shape)
-# print(testX.shape, testY.shape)
-
-# LSTM model
-model = Sequential()
-model.add(LSTM(64, input_shape=(trainX.shape[1], trainX.shape[2]), # (seq length, input dimension)
-               return_sequences=True))
-model.add(LSTM(32, return_sequences=False))
-model.add(Dense(trainY.shape[1]))
-
-model.summary()
-
-# specify your learning rate
-learning_rate = 0.01
-# create an Adam optimizer with the specified learning rate
-optimizer = Adam(learning_rate=learning_rate)
-# compile your model using the custom optimizer
-model.compile(optimizer=optimizer, loss='mse')
-
-# Try to load weights
-try:
-    model.load_weights('./save_weights/lstm_weights.h5')
-    print("Loaded model weights from disk")
-except:
-    print("No weights found, training model from scratch")
-    # Fit the model
-    history = model.fit(trainX, trainY, epochs=30, batch_size=32,
-                    validation_split=0.1, verbose=1)
-    # Save model weights after training
-    model.save_weights('./save_weights/lstm_weights.h5')
-
-    plt.plot(history.history['loss'], label='Training loss')
-    plt.plot(history.history['val_loss'], label='Validation loss')
-    plt.legend()
-    plt.show()
+df.head()
 
 
-# prediction
-prediction = model.predict(testX)
-print(prediction.shape, testY.shape)
+# data check
+fig = df['Close'].plot()
 
-# generate array filled with means for prediction
-mean_values_pred = np.repeat(scaler.mean_[np.newaxis, :], prediction.shape[0], axis=0)
+X = df.drop('Close', axis=1) # X, y 분리
+y = df[['Close']]
 
-# substitute predictions into the first column
-mean_values_pred[:, 0] = np.squeeze(prediction)
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-# inverse transform
-y_pred = scaler.inverse_transform(mean_values_pred)[:,0]
-print(y_pred.shape)
+ms = MinMaxScaler() # 0 ~ 1
+ss = StandardScaler() # 평균 0, 분산 1
 
-# generate array filled with means for testY
-mean_values_testY = np.repeat(scaler.mean_[np.newaxis, :], testY.shape[0], axis=0)
+X_ss = ss.fit_transform(X)
+y_ms = ms.fit_transform(y)
 
-# substitute testY into the first column
-mean_values_testY[:, 0] = np.squeeze(testY)
+X_train = X_ss[:79, :]
+X_test = X_ss[79:, :]
 
-# inverse transform
-testY_original = scaler.inverse_transform(mean_values_testY)[:,0]
-print(testY_original.shape)
+y_train = y_ms[:79, :]
+y_test = y_ms[79:, :]
 
-# plotting
-plt.figure(figsize=(14, 5))
+print('Training Shape :', X_train.shape, y_train.shape)
+print('Testing Shape :', X_test.shape, y_test.shape)
 
-# plot original 'Open' prices
-plt.plot(dates, original_open, color='green', label='Original Open Price')
 
-# plot actual vs predicted
-plt.plot(test_dates[seq_len:], testY_original, color='blue', label='Actual Open Price')
-plt.plot(test_dates[seq_len:], y_pred, color='red', linestyle='--', label='Predicted Open Price')
-plt.xlabel('Date')
-plt.ylabel('Open Price')
-plt.title('Original, Actual and Predicted Open Price')
-plt.legend()
-plt.show()
+# 데이터셋 형태 및 크기 조정
+X_train_tensors = Variable(torch.Tensor(X_train))
+X_test_tensors = Variable(torch.Tensor(X_test))
 
-# Calculate the start and end indices for the zoomed plot
-zoom_start = len(test_dates) - 50
-zoom_end = len(test_dates)
+y_train_tensors = Variable(torch.Tensor(y_train))
+y_test_tensors = Variable(torch.Tensor(y_test))
 
-# Create the zoomed plot
-plt.figure(figsize=(14, 5))
+X_train_tensors_f = torch.reshape(X_train_tensors, 
+                                  (X_train_tensors.shape[0], 1, X_train_tensors.shape[1]))
 
-# Adjust the start index for the testY_original and y_pred arrays
-adjusted_start = zoom_start - seq_len
+X_test_tensors_f = torch.reshape(X_test_tensors,
+                                (X_test_tensors.shape[0], 1, X_test_tensors.shape[1]))
 
-plt.plot(test_dates[zoom_start:zoom_end],
-         testY_original[adjusted_start:zoom_end - zoom_start + adjusted_start],
-         color='blue',
-         label='Actual Open Price')
+print('Training Shape :', X_train.shape, y_train.shape)
+print('Testing Shape :', X_test.shape, y_test.shape)
 
-plt.plot(test_dates[zoom_start:zoom_end],
-         y_pred[adjusted_start:zoom_end - zoom_start + adjusted_start ],
-         color='red',
-         linestyle='--',
-         label='Predicted Open Price')
+    
 
-plt.xlabel('Date')
-plt.ylabel('Open Price')
-plt.title('Zoomed In Actual vs Predicted Open Price')
+num_epochs = 1000
+learning_rate = 0.0001
+
+input_size=5
+hidden_size=2
+num_layers=1
+
+num_classes=1
+model=GRU(num_classes,input_size,hidden_size,num_layers,X_train_tensors_f.shape[1])
+
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+for epoch in range(num_epochs) :
+    outputs = model.forward(X_train_tensors_f)
+    optimizer.zero_grad()
+    loss = criterion(outputs, y_train_tensors)
+    loss.backward()
+    
+    optimizer.step()
+    if epoch % 100 == 0 :
+        print(f'Epoch : {epoch}, loss : {loss.item():1.5f}')
+
+df_x_ss = ss.transform(X)
+df_y_ms = ms.transform(y)
+
+df_x_ss = Variable(torch.Tensor(df_x_ss))
+df_y_ms = Variable(torch.Tensor(df_y_ms))
+df_x_ss = torch.reshape(df_x_ss, (df_x_ss.shape[0], 1, df_x_ss.shape[1]))
+
+train_predict = model(df_x_ss)
+predicted = train_predict.data.numpy()
+
+label_y = df_y_ms.data.numpy()
+
+predicted = ms.inverse_transform(predicted)
+label_y = ms.inverse_transform(label_y)
+plt.figure(figsize=(10, 6))
+plt.axvline(x=datetime(2022,5,1), c='r', linestyle='--')
+
+df['pred'] = predicted
+plt.plot(df['Close'], label='Actual Data')
+plt.plot(df['pred'], label='Predicted Data')
+
+plt.title('Time-series Prediction')
 plt.legend()
 plt.show()
